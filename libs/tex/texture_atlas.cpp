78d8729e121ccfc30c6a493d32b0d2fda490238d
jakebobu <jakebobula@gmail.com>
2019-12-18 12:26:11 -0700

    bool

--- libs/tex/texture_atlas.cpp
+++ libs/tex/texture_atlas.cpp
@@ -10,7 +10,7 @@
 #include <set>
 #include <map>
 #include <string>
-#include <utility> 
+#include <utility>
 #include <util/file_system.h>
 #include <mve/image_tools.h>
 #include <mve/image_io.h>
@@ -87,7 +87,7 @@ TextureAtlas::insert(TexturePatch::ConstPtr texture_patch) {
     math::Vec2f offset = math::Vec2f(rect.min_x + padding, rect.min_y + padding);
 
     std::cout << "line 88 of texture_atlas.cpp next line is faces.insert(faces.end(), patch_faces.begin(), patch_faces.end())" << std::endl;
-    std::pair <std::string,double> val;
+    std::pair <std::string,bool> val;
     val = faces.insert(faces.end(), patch_faces.begin(), patch_faces.end());
     std::cout << val.first << val.second << std::endl;
 
@@ -139,7 +139,7 @@ TextureAtlas::apply_edge_padding(void) {
 
                         /* Add the pixel to the set of invalid border pixels. */
                         std::cout << "line 138 of texture_atlas.cpp next line is invalid_border_pixels.insert(std::pair<int, int>(x, y))" << std::endl;
-                        std::pair <std::string,double> val;
+                        std::pair <std::string,bool> val;
                         val = invalid_border_pixels.insert(std::pair<int, int>(x, y));
                         std::cout << val.first << val.second << std::endl;
                     }
@@ -215,7 +215,7 @@ TextureAtlas::apply_edge_padding(void) {
                          new_validity_mask->at(nx, ny, 0) == 0) {
 
                          std::cout << "line 212 of texture_atlas.cpp next line is invalid_border_pixels.insert(std::pair<int, int>(nx, ny))" << std::endl;
-                         std::pair <std::string,double> val;
+                         std::pair <std::string,bool> val;
                          val = invalid_border_pixels.insert(std::pair<int, int>(nx, ny));
                          std::cout << val.first << val.second << std::endl;
                     }
